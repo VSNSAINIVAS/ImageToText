8cc6c89ba590316fb3d79c43a09736608f44da35
Nivas <vsnsainivasand2003@gmail.com>
2022-10-16 11:51:24 +0530

    Git file added

--- Image/settings.py
+++ Image/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
