b681745c5c33d7718783c5ef48583b4e437b9236
Nivas <vsnsainivasand2003@gmail.com>
2022-10-16 12:39:43 +0530

    Made changes

--- toText/Teserract/tecessaract.py
+++ toText/Teserract/tecessaract.py
@@ -12,7 +12,7 @@ nltk.download('stopwords')
 
 def teserract(url):
     #Define path to tessaract.exe
-    path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    path_to_tesseract = r"tesseract.exe"
 
     #Define path to image   
     response = requests.get(url)
